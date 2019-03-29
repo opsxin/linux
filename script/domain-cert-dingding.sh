a35cbc6f226cd2d51f9dd75007b91a122483083e
opsxin <haoxin0880@gmail.com>
2019-03-29 17:55:18 +0800

    dingding alert for domain cert expired time

---
+++ script/domain-cert-dingding.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOMAINS=("www.baidu.com")
+DINGDING="https://oapi.dingtalk.com/robot/send?access_token="
+HEAD='{"msgtype":"markdown", "markdown":{"title":"域名证书信息", "text":"'
+TAIL='"}}'
+
+for domain in ${DOMAINS[@]}
+do
+    expired_time=$(timeout 2 openssl s_client -connect ${domain}:443 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f2)
+    expired_time_format=$(date -d "${expired_time}" "+%Y/%m/%d %H:%M:%S")
+
+    d1=$(date +%s -d ${expired_time_format%\ *})
+    d2=$(date +%s -d $(date +%F))
+    days=$(((d1-d2)/86400))
+
+    body="查询域名：${domain} \n - **到期时间**：${expired_time_format} \n - **距今还有**：${days}天 [详情](https://myssl.com/${domain}) \n"
+    full_info=${HEAD}${body}${TAIL}
+    curl -s "${DINGDING}" -H 'Content-Type: application/json' -d "${full_info}"
+done
+
