10ae478f8281b70366205c15a6801016e6748f56
opsxin <haoxin0880@gmail.com>
2019-05-22 15:17:28 +0800

    script: send-msg-to-wechat: add descript

--- script/send-msg-to-wechat.sh
+++ script/send-msg-to-wechat.sh
@@ -3,7 +3,7 @@
 URL="https://sc.ftqq.com/YOU_KEY.send"
 
 #text标题
-[ -n "$1" ] && TEXT=$1 || { echo "$0 TEXT [DESP]"; exit 1; }
+[ -n "$1" ] && TEXT=$1 || { echo "usage: $0 TEXT [DESP]"; exit 1; }
 #desp内容
 [ -n "$2" ] && DESP=$2 || DESP="![Worship](https://i.loli.net/2019/04/18/5cb7ddd2324a6.gif)"
 
