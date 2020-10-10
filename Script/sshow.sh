8691808600ed31317ec44599a1c27b8e01ab7bda
opsxin <haoxin0880@gmail.com>
2020-10-10 14:42:29 +0800

    Script: sshow 支持自己添加 ssh 连接参数

--- Script/sshow.sh
+++ Script/sshow.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+parameter="$@"
+
 test -f ~/.ssh/config || ( echo "未存在 config 文件" && exit 1 )
 
 hosts+=($(awk '/^Host/{print $2}' ~/.ssh/config))
 
 PS3="选择要连接的主机 ID: "
 select host in "${hosts[@]}"; do
-    ssh -o ConnectTimeout=10 "${host}"
+    ssh -o ConnectTimeout=10 ${parameter} "${host}"
     break
 done
