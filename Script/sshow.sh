f81d23a8c6c2afcf2413d2273db389116c7dc670
opsxin <haoxin0880@gmail.com>
2020-10-30 16:50:50 +0800

    Script: 选择 ssh 主机不存在退出脚本

--- Script/sshow.sh
+++ Script/sshow.sh
@@ -2,12 +2,13 @@
 
 parameter="$@"
 
-test -f ~/.ssh/config || ( echo "未存在 config 文件" && exit 1 )
+test -f ~/.ssh/config || ( echo "未存在 SSH config 文件" && exit 1 )
 
-hosts+=($(awk '/^Host/{print $2}' ~/.ssh/config))
+hosts+=($(awk '/^[Hh]ost/{print $2}' ~/.ssh/config))
 
 PS3="选择要连接的主机 ID: "
 select host in "${hosts[@]}"; do
+    if [ -z "${host}" ]; then break; fi
     ssh -o ConnectTimeout=10 ${parameter} "${host}"
     break
 done
